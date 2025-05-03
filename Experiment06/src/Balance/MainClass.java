9309fac82db15872de0a680a0fe86611dda81dde
Kashisharora12 <arorakashish832005@gmail.com>
2025-05-03 12:34:43 +0530

    Add files via upload

---
+++ Experiment06/src/Balance/MainClass.java
@@ -0,0 +1,9 @@
+package Balance;
+
+public class MainClass {
+    public static void main (String[] args)
+    {  	
+    	Account account = new Account(5000);
+    	account.Display_Balance();
+    }
+}
